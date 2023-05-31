8add8e72227c0d30b1bc67e4ca7155cf7cc57a9c
Rkornilov98 <rkornilov98@mail.ru>
2023-05-31 20:28:13 +0600

    Добавлены скрипты инициализации БД

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into Product(series, producer, price, amount, type, property)
-values(12345678, 'LG', 15000, 3, 'NOTEBOOK', 'Size:17');
+values(12345678, 'LG', 15000, 4, 'MONITOR', '21.0');
+insert into Product(series, producer, price, amount, type, property)
+values(23456789, 'Toshiba', 15000, 11, 'HARD_DISC', '1024');
+insert into Product(series, producer, price, amount, type, property)
+values(34567891, 'HP', 55000, 3, 'NOTEBOOK', '17');
+insert into Product(series, producer, price, amount, type, property)
+values(45678912, 'Asus', 45000, 1, 'DESKTOP_COMPUTER', 'DESKTOP');
